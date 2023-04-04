508dc3fb6d4ea4115b88f75f4175de9bc19300fc
Lori <shilong.li213@gmail.com>
2023-04-03 17:05:04 -0700

    [assignment1] finished

--- week_1/main.go
+++ week_1/main.go
@@ -5,21 +5,39 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"regexp"
 	"sort"
 	"strings"
 )
 
-func main() {
+type words struct {
+	Word string
+	Freq int32
+}
 
+func main() {
 	path := os.Args[1]
 	fp, err := os.Open(path)
+	stopWordsPath := "/Volumes/mac-hdd/projects/APL-go/stop_words.txt"
 	if err != nil {
 		panic(err)
 	}
 	r := bufio.NewReader(fp)
 	dict := map[string]int32{}
+	stopWords, err := os.Open(stopWordsPath)
+	if err != nil {
+		panic(err)
+	}
+	stopWordsSet := map[string]bool{}
+	stopWordsR := bufio.NewReader(stopWords)
+	stopWordsLine, err := stopWordsR.ReadBytes('\n')
+	stopWordsArr := strings.Split(string(stopWordsLine), ",")
+	for i := 0; i < len(stopWordsArr); i++ {
+		stopWordsSet[stopWordsArr[i]] = true
+	}
+	reg_exp := regexp.MustCompile("[0-9a-zA-Z]+")
 	for {
-		lineBytes, err := r.ReadBytes('\n')
+		lineBytes, err := r.ReadBytes('\n') // "word--word"
 		line := strings.TrimSpace(string(lineBytes))
 		if err != nil && err != io.EOF {
 			panic(err)
@@ -27,25 +45,29 @@ func main() {
 		if err == io.EOF {
 			break
 		}
-		arr := strings.Split(line, "[^0-9a-zA-Z]+")
+		arr := strings.Fields(line)
 		for i := 0; i < len(arr); i++ {
-			v, ok := dict[arr[i]]
-			if ok {
-				dict[arr[i]] = v + 1
-			} else {
-				dict[arr[i]] = 1
+			w := reg_exp.FindAllString(arr[i], -1)
+			for i := 0; i < len(w); i++ {
+				w[i] = strings.ToLower(w[i])
+				if stopWordsSet[w[i]] || len(w[i]) < 2 {
+					continue
+				}
+				v, ok := dict[w[i]]
+				if ok {
+					dict[w[i]] = v + 1
+				} else {
+					dict[w[i]] = 1
+				}
 			}
 		}
 	}
 
-	type words struct {
-		Word string
-		Freq int32
-	}
 	var lstWords []words
 	for k, v := range dict {
 		lstWords = append(lstWords, words{k, v})
 	}
+
 	sort.Slice(lstWords, func(i, j int) bool {
 		if lstWords[i].Freq == lstWords[j].Freq {
 			return lstWords[i].Word < lstWords[j].Word
@@ -53,13 +75,17 @@ func main() {
 			return lstWords[i].Freq > lstWords[j].Freq
 		}
 	})
-	var minNum = int32(25)
-	for i := 0; i < min(minNum, int32(len(lstWords))); i++ {
-		fmt.Printf("%s  -  %d\n", lstWords[i].Word, lstWords[i].Freq)
+
+	minNum := 25
+	for i := 0; i < min(minNum, len(lstWords)); i++ {
+		fmt.Printf("%s  -  %d", lstWords[i].Word, lstWords[i].Freq)
+		if i != min(minNum, len(lstWords))-1 {
+			fmt.Println()
+		}
 	}
 }
 
-func min(i int32, j int32) int32 {
+func min(i int, j int) int {
 	if i > j {
 		return j
 	} else {
